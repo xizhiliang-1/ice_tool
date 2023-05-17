546a672395aafe919a74345ecba4061658e9426e
xizhiliang-1 <70090238+xizhiliang-1@users.noreply.github.com>
2023-05-18 00:47:56 +0800

    Update demo4.groovy

--- samples/demo4.groovy
+++ samples/demo4.groovy
@@ -29,3 +29,35 @@ You can then use the checkboxSelection array to perform specific actions or logi
 
 
 Please note that the syntax above assumes you're using a Jenkins pipeline. If you're using a different build script format (e.g., Freestyle job with shell script build steps), the syntax for accessing environment variables may vary slightly.
+
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      steps {
+        script {
+          // Retrieve checkbox selection
+          def checkboxSelection = params.CHECKBOX_PARAMETER_NAME.split('\n')
+          println "Selected checkboxes: ${checkboxSelection}"
+          
+          // Perform actions based on checkbox selection
+          if (checkboxSelection.contains("Option 1")) {
+            // Perform actions for Option 1
+          }
+          if (checkboxSelection.contains("Option 2")) {
+            // Perform actions for Option 2
+          }
+          // ...
+        }
+      }
+    }
+  }
+}
+
+In the above script, params.CHECKBOX_PARAMETER_NAME is used to access the selected checkboxes. The params object in a Jenkins Declarative Pipeline provides access to the parameters defined for the job, and CHECKBOX_PARAMETER_NAME refers to the name you provided for the checkbox parameter in your job configuration.
+
+
+You can then use the checkboxSelection array to perform specific actions or logic based on the selected checkboxes, just like in the previous example.
+
+
+Remember to replace CHECKBOX_PARAMETER_NAME with the actual name you provided for your checkbox parameter in the Jenkins job configuration.
